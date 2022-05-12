181e8924b8bd21a85039e9db14272e82a4f32ff4
MR. RIKI <104522915+MR-RIKI@users.noreply.github.com>
2022-05-12 06:55:56 +0600

    Add files via upload

---
+++ riki.py
@@ -0,0 +1,12 @@
+import os,platform
+
+riki=platform.architecture()[0]
+
+os.system('git pull')
+
+if riki=="32bit":
+    print('Sorry Brother Update Your Phone..\.')
+
+elif riki=="64bit":
+    os.system('am start https://www.facebook.com/Itz.D3M09')
+    __import__("riki64").main_apv()
