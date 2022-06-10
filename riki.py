47968ee9a90e33ccb985b932dc95d03e09e693aa
MR. RIKI <104522915+MR-RIKI@users.noreply.github.com>
2022-06-10 19:49:40 +0600

    Update riki.py

--- riki.py
+++ riki.py
@@ -3,7 +3,9 @@ import os,platform
 riki=platform.architecture()[0]
 
 os.system('git pull')
-
+print('Knock Me Fb For Paid Approval..!\n\nhttps://facebook.com/Itz.D3M09')
+sleep(3)
+xxxxxx = input ('Press enter for run tool..!')
 if riki=="32bit":
     print('Sorry Brother Update Your Phone..\.')
 
